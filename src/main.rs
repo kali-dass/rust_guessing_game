92a8937f176d1c9e670448d1f9a150fb296e4e3b
Kalidass Mookkaiah <kalidass@gmail.com>
2023-03-30 01:35:36 +0100

    loop and break of match

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
@@ -8,21 +8,26 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess:");
+    loop {
+        println!("Please input your guess:");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-    .read_line(&mut guess)
-    .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
+        let guess: u32 = guess.trim().parse().expect("Please type a number");
 
-    print!("Your guessed: {guess}");
+        print!("Your guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => print!("Too small"),
-        Ordering::Greater => print!("Too big"),
-        Ordering::Equal => print!("You win"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => print!("Too small"),
+            Ordering::Greater => print!("Too big"),
+            Ordering::Equal => {
+                print!("You win");
+                break;
+            }
+        }
     }
 }
